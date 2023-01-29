7467fbdabeac7dc2c1ba1f8b54ab71960fdf3647
Zaruma Denys <121362477+zaruma-denys@users.noreply.github.com>
2023-01-29 23:04:10 +0200

    Add files via upload

---
+++ HM 12/script1.js
@@ -0,0 +1,25 @@
+var city= 'Київ';
+ city= 'Львів';
+
+ let adress;
+ adress = city;
+
+ alert(adress);
+
+
+let n = 10;
+let result = Math.pow(n , 3);
+
+console.log(result);
+
+let pen = 4;
+let penprice = 5.25;
+
+let pen4price = pen * penprice;
+
+
+let pencil = 6;
+let pencilprice = 3.50;
+
+let pencil6price = pencil * pencilprice;
+document.write("Ціна за олівці:" + pencil6price + ', ' + "Ціна за ручки:" + pen4price + '.')
